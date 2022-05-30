bd635b86a9e97f93c5079ee01985502696beba6f
Tori Taylor <tori.taylor19@gmail.com>
2022-05-29 21:45:09 -0400

    Adding Dev

--- plots.js
+++ plots.js
@@ -1,37 +1,6 @@
+// Init function takes ID num and sets them as
+// options of the dropdown menu
 function init() {
-    data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] 
-    }];
-    Plotly.newPlot("plot", data);
-  };
-  
-  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-  function updatePlotly() {
-    var dropdownMenu = d3.select("#dropdownMenu");
-    var dataset = dropdownMenu.property("value");
-  
-    var xData = [1, 2, 3, 4, 5];
-    var yData = [];
-  
-    if (dataset === 'dataset1') {
-      yData = [1, 2, 4, 8, 16];
-    };
-  
-    if (dataset === 'dataset2') {
-      yData = [1, 10, 100, 1000, 10000];
-    };
-  
-    var trace = {
-      x: [xData],
-      y: [yData],
-    };
-    Plotly.restyle("plot", trace);
-  };
-  
-  init();
-
-  function init() {
     var selector = d3.select("#selDataset");
   
     d3.json("samples.json").then((data) => {
@@ -46,19 +15,29 @@ function init() {
   })}
   
   init();
-function optionChanged(newSample) {
+
+  // optionChanged function is called in html
+  function optionChanged(newSample) {
+    console.log(newSample);
     buildMetadata(newSample);
     buildCharts(newSample);
-}
+  }
 
-function buildMetadata(sample) {
+  // buildMetadata function called in optionChanged
+  function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray[0];
+      var resultsArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultsArray[0]; //Because results are returned like an array =[0]
       var PANEL = d3.select("#sample-metadata");
-  
-      PANEL.html("");
-      PANEL.append("h6").text(result.location);
+
+      PANEL.html(""); //Clear content before other id gets called
+      PANEL.append("h6").text(`ID: ${result.id}`);
+      PANEL.append("h6").text(`Ethnicity: ${result.ethnicity}`);
+      PANEL.append("h6").text(`Gender: ${result.gender}`);
+      PANEL.append("h6").text(`Age: ${result.age}`);
+      PANEL.append("h6").text(`Location: ${result.location}`);
+      PANEL.append("h6").text(`BBtype: ${result.bbtype}`);
+      PANEL.append("h6").text(`Washing/week: ${result.wfreq}`);
     });
   }
